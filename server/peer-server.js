f42cf1dc6fd17915f391397eab59bfd440d356e5
Yuri Petrik <yuri.petrik@gmail.com>
2019-07-27 04:18:40 +0300

    Update peer-server.js

--- server/peer-server.js
+++ server/peer-server.js
@@ -3,7 +3,7 @@ var PeerServer = require('peer').PeerServer;
 
 var server = PeerServer({
     port: 9000,
-    path: '/peerjs',
+    path: '/',
     ssl: {
         key: fs.readFileSync('./../certificates/key.pem', 'utf8'),
         cert: fs.readFileSync('./../certificates/cert.pem', 'utf8')
